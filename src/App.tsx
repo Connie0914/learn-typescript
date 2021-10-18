23608c29478e8e5c6e7c4ae46bfa152faec9bfd1
こにー | Connie <71988605+Connie0914@users.noreply.github.com>
2021-10-18 23:07:53 +0900

    変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { Practice1 } from "./Practices/Practice1";
+import { Practice2 } from "./Practices/Practice2";
 import "./styles.css";
 
 export default function App() {
@@ -9,6 +11,8 @@ export default function App() {
   };
   return (
     <div className="App">
+      <Practice1 />
+      <Practice2 />
       <button onClick={onClickFetchData}>データ取得</button>
     </div>
   );
